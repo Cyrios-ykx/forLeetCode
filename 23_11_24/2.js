f85a27889d003a0eeb07054d6aa17ba062312ea9
Cyrios <influx@influxdeMac-mini.local>
2023-11-24 17:25:30 +0800

    方法重写

--- 23_11_24/2.js
+++ 23_11_24/2.js
@@ -33,9 +33,11 @@
 
 // 循序渐进
 function getStackString() {
-  // const s = '2[bc]'
-  // const s = '2[bc]2[ac]'
-  //
+  // const s = '2[bc]' ✅
+  // const s = '2[bc]2[ac]' ✅
+  const s = '2[abc]3[cd]ef'
+  // const s = "abc3[cd]xyz"
+  // const s = "3[a2[c]]"
   let stack = []
   let allString = ''
   for (let i = 0; i < s.length; i++) {
